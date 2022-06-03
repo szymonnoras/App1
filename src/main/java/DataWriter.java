92a9820500f1fa4f2da321d0f82c61cc6f6f3c07
szymonnoras <saronomysz@o2.pl>
2022-06-03 13:25:36 +0200

    Data writer
    Implement writing data from list to xml file.

---
+++ src/main/java/DataWriter.java
@@ -0,0 +1,62 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.List;
+
+public class DataWriter {
+    public static void writeDataToXML(List<String[]> data , String path) throws ParserConfigurationException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        Document document = builder.newDocument();
+        Element rootElement = document.createElement("cards");
+        document.appendChild(rootElement);
+
+        for (int i = 1; i < data.size(); i++) {
+            Element card = document.createElement("card");
+            rootElement.appendChild(card);
+
+            Element name=document.createElement("name");
+            name.appendChild(document.createTextNode(data.get(i)[0]));
+            card.appendChild(name);
+
+            Element surname=document.createElement("surname");
+            surname.appendChild(document.createTextNode(data.get(i)[1]));
+            card.appendChild(surname);
+
+            Element phone=document.createElement("phone");
+            phone.appendChild(document.createTextNode(data.get(i)[2]));
+            card.appendChild(phone);
+        }
+
+
+        try (FileOutputStream outputStream = new FileOutputStream(path)){
+            writeXml(document, outputStream);
+        }  catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void writeXml(Document document, OutputStream output) throws TransformerException {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+        DOMSource source = new DOMSource(document);
+        StreamResult result = new StreamResult(output);
+
+        transformer.transform(source, result);
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,15 @@
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
 import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParserConfigurationException {
         String path="src/main/resources/data.csv";
+        String pathXML="src/main/resources/dataaa.xml";
         List<String[]> data=DataReader.readData(path);
-        DataReader.printData(data);
-        DataReader.readData("asd");
+        //DataReader.printData(data);
+        //DataReader.readData("asd");
+        DataWriter.writeDataToXML(data,pathXML);
     }
 }
